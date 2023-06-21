8309cb9b36c1daa4268021de5e18764fd2cc468c
David Vavra <david@vavra.me>
2023-06-21 22:59:58 +0200

    Update database.ts

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -3,14 +3,33 @@ import { DataSnapshot } from "firebase-functions/lib/v1/providers/database";
 
 export async function doProcessRating(snap: DataSnapshot) {
   const rating = snap.val();
-  const fromNameGenitiv = (await admin.database().ref("nearbyUsers/" + rating.from + "/nameGenitiv").once("value")).val()
+  const raterUser = (await admin.database().ref("nearbyUsers/" + rating.from).once("value")).val()
+  // change rating
+  const raterRating = raterUser.totalRating
+  const weight = raterRating < 1 ? 0 : raterRating < 2 ? 1 : raterRating < 3 ? 2 : raterRating < 4 ? 3 : raterRating < 4.5 ? 4 : 5
+  // const ratedUser = (await admin.database().ref("nearbyUsers/" + rating.to).once("value")).val()
+  // const newRatingCount = ratedUser.ratingCount + weight
+  // const newRating = (ratedUser.totalRating * ratedUser.ratingCount + rating.stars * weight) / newRatingCount
+  // await admin.database().ref("nearbyUsers/" + rating.to).update({
+  //   'ratingCount': newRatingCount,
+  //   'totalRating': newRating
+  // })
+  await admin.database().ref("nearbyUsers/" + rating.to).transaction(
+    ratedUser => {
+      if (ratedUser == null) return null
+      ratedUser.ratingCount = ratedUser.ratingCount + weight
+      raterUser.totalRating = (ratedUser.totalRating * ratedUser.ratingCount + rating.stars * weight) / raterUser.ratingCount
+      return ratedUser
+    }
+  )
+  // send notification
   const token = (await admin.database().ref("userSecrets/" + rating.to + "/notificationsToken").once("value")).val()
   const androidConfig: admin.messaging.AndroidConfig = {
     priority: 'high'
   }
   const message = {
     data: {
-      fromNameGenitiv: fromNameGenitiv,
+      fromNameGenitiv: raterUser.fromNameGenitiv,
       stars: String(rating.stars)
     },
     android: androidConfig,
