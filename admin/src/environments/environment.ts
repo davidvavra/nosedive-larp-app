e3a0e9cc93160296bf4759b79d0b47f08a206633
David Vávra <david@vavra.me>
2023-06-27 22:37:13 +0200

    Security

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
     measurementId: 'G-7R4TLS3JQM',
   },
   slack: {
-    botToken: 'xoxb-5482843366724-5490041629285-ZQe9VngyHxsLo9LgBYRMDLbV'
+    botToken: 'bot_token_from_ci'
   }
 };
