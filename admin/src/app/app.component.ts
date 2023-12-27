d55e456cb0d283dc8882f2641b7d345e460fa969
David Vavra <david@vavra.me>
2023-12-27 17:09:38 +0100

    Fix bug with reloading

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -14,12 +14,16 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
   title = 'admin';
   private database: Database = inject(Database);
-  users: Observable<User[] | null>;
+  users: User[] = [];
   channels: Observable<Channel[]>;
   model = new SlackMessage(new User("", ""), new Channel("", ""), "")
 
   constructor(private http: HttpClient) {
-    this.users = listVal(query(ref(this.database, "nearbyUsers")))
+    listVal(query(ref(this.database, "nearbyUsers"))).subscribe(users => {
+      if (users != null && !this.isSame(users as User[], this.users)) {
+        this.users = users as User[]
+      }
+    })
     this.channels = http.post<Channels>("https://slack.com/api/conversations.list?types=public_channel%2C%20private_channel", "token=" + environment.slack.botToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).pipe(map(channels => channels.channels))
   }
 
@@ -27,6 +31,11 @@ export class AppComponent {
     this.http.post("https://slack.com/api/chat.postMessage?channel=" + this.model.channel.id + "&icon_url=" + encodeURI(this.model.user.profilePictureUrl) + "&text=" + encodeURI(this.model.text) + "&username=" + this.model.user.name, "token=" + environment.slack.botToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).subscribe(response => {
       console.log(JSON.stringify(response))
     })
+  }
+
+  isSame(first: User[], second: User[]): Boolean {
+    return first.length === second.length &&
+    first.every((element, index) => element.name === second[index].name && element.profilePictureUrl === second[index].profilePictureUrl);
   }
 }
 
@@ -61,3 +70,4 @@ export class Channel {
   ) { }
 
 }
+
