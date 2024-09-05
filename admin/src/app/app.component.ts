62637b065c534bcb14b2d031721e6b95510799b1
David Vávra <david@vavra.me>
2024-09-05 15:13:32 +0200

    Reporting to db

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable, inject } from '@angular/core';
-import { Database, listVal, query, ref } from '@angular/fire/database';
+import { Database, listVal, query, ref, push } from '@angular/fire/database';
 import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
@@ -17,11 +17,12 @@ export class AppComponent {
   npcs: User[] = [];
   players: User[] = [];
   channels: Observable<Channel[]>;
-  model = new SlackMessage(new User("", "", ""), new Channel("", ""), "", new User("", "", ""), new User("", "", ""), new User("", "", ""))
+  state = new State(NO_USER, new Channel("", ""), "", NO_USER, NO_USER, NO_USER, 0.1, 0.1, "")
 
   constructor(private http: HttpClient) {
     listVal(query(ref(this.database, "nearbyUsers")), { keyField: "id" }).subscribe(users => {
       if (users != null) {
+        users.push(NO_USER)
         let npcUsers = (users as User[]).filter(user => user.id.startsWith("_")).sort((a, b) => a.name.localeCompare(b.name))
         let playerUsers = (users as User[]).filter(user => !user.id.startsWith("_")).sort((a, b) => a.name.localeCompare(b.name))
         if (!this.isSame(npcUsers, this.npcs)) {
@@ -35,12 +36,23 @@ export class AppComponent {
     this.channels = http.post<Channels>("https://slack.com/api/conversations.list?types=public_channel%2C%20private_channel", "token=" + environment.slack.botToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).pipe(map(channels => channels.channels))
   }
 
-  onSubmit() {
-    let url = "https://slack.com/api/chat.postMessage?channel=" + this.model.channel.id + "&icon_url=" + encodeURIComponent(this.model.user.profilePictureUrl) + "&text=" + encodeURIComponent(this.model.text) + "&username=" + this.model.user.name
+  onMessageSubmit() {
+    let url = "https://slack.com/api/chat.postMessage?channel=" + this.state.channel.id + "&icon_url=" + encodeURIComponent(this.state.user.profilePictureUrl) + "&text=" + encodeURIComponent(this.state.text) + "&username=" + this.state.user.name
     console.log("url=" + url)
     this.http.post(url, "token=" + environment.slack.botToken, { headers: { "Content-Type": "application/x-www-form-urlencoded" } }).subscribe(response => {
       console.log(JSON.stringify(response))
-      this.model.text = ""
+      this.state.text = ""
+    })
+  }
+
+  onReportSubmit() {
+    push(ref(this.database, "reports"), {
+      "reporter1": this.state.reporter1.id,
+      "reporter2": this.state.reporter2.id,
+      "victim": this.state.victim.id,
+      "penalty": this.state.penalty,
+      "reward": this.state.reward,
+      "reason": this.state.reason
     })
   }
 
@@ -50,7 +62,7 @@ export class AppComponent {
   }
 }
 
-export class SlackMessage {
+export class State {
 
   constructor(
     public user: User,
@@ -58,7 +70,10 @@ export class SlackMessage {
     public text: string,
     public reporter1: User,
     public reporter2: User,
-    public victim: User
+    public victim: User,
+    public penalty: number,
+    public reward: number,
+    public reason: string
   ) { }
 
 }
@@ -72,6 +87,8 @@ export class User {
   ) { }
 
 }
+
+let NO_USER = new User("unknown", "-- Nikdo --", "")
 
 export interface Channels {
   channels: Channel[]
